169b5d298ca058ebc3657acc5f6474c57aa287db
Matthew Arp <marp@huntongroup.com>
2019-10-25 16:49:25 -0500

    Name/Spelling Corrections

--- Library/GenericAdditionalMenus.cs
+++ Library/GenericAdditionalMenus.cs
@@ -6,28 +6,28 @@ namespace Microsoft.Dexterity.Bridge.Extended
 
     public static class GenericAdditionalMenus
     {
-        public static event GenericAdditionalMenuHandler MenuClicked;
+        public static event GenericAdditionalMenuHandler GenericAdditionalMenuClicked;
 
-        public static Guid Register(short productId, string formName, string menuText, string acceleratorKey)
+        public static Guid Add(short productId, string formName, string menuText, string acceleratorKey)
         {
             Guid eventTag = Guid.NewGuid();
             DictionaryRoot root = new DictionaryRoot(productId, false);
-            root.Forms[formName].AddMenuHandler((sender, e) => OnMenuClicked((DictionaryForm)sender, eventTag), menuText, acceleratorKey);
+            root.Forms[formName].AddMenuHandler((sender, e) => FireEvent((DictionaryForm)sender, eventTag), menuText, acceleratorKey);
 
             return eventTag;
         }
 
-        public static Guid GenericAdditonalMenu(this DictionaryForm form, string menuText, string acceleratorKey)
+        public static Guid GenericAdditionalMenu(this DictionaryForm form, string menuText, string acceleratorKey)
         {
             Guid eventTag = Guid.NewGuid();
-            form.AddMenuHandler((sender, e) => OnMenuClicked((DictionaryForm)sender, eventTag), menuText, acceleratorKey);
+            form.AddMenuHandler((sender, e) => FireEvent((DictionaryForm)sender, eventTag), menuText, acceleratorKey);
 
             return eventTag;
         }
 
-        private static void OnMenuClicked(DictionaryForm form, Guid eventTag)
+        private static void FireEvent(DictionaryForm form, Guid eventTag)
         {
-            MenuClicked?.Invoke(form, eventTag);
+            GenericAdditionalMenuClicked?.Invoke(form, eventTag);
         }
     }
 }
